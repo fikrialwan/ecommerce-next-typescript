7c7c3cb2d64d9472310c2823f71f88ebcbcaeed1
fikrialwan <fikriar93@gmail.com>
2022-08-23 15:11:12 +0700

    move fetch api

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,26 +1,5 @@
-import { type } from "os";
 import getAllProductsQuery from "../utils/queries/get-all-products";
-
-type FetchParams = {
-    query: string
-}
-
-const fetchAPI = async ({ query }: FetchParams) => {
-    const url = "http://localhost:4000/graphql";
-
-    const res = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            query
-        })
-    });
-
-    const data = await res.json();
-    return { data };
-}
+import fetchAPI from "../utils/fetch-api";
 
 const getAllProducts = async (): Promise<any[]> => {
     const products = await fetchAPI({ query: getAllProductsQuery });
